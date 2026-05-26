12be27e8f8ee5ebb00f70286f35de8fd8abee8a5
kuldeep <kuldeep@insonix.com>
2026-05-26 17:53:58 +0530

    minor fixes

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -2,9 +2,9 @@ import { BlockRenderer } from '@/components/BlockRenderer'
 import FooterRenderer from '@/components/footer/FooterRenderer'
 import HeaderRenderer from '@/components/headers/HeaderRenderer'
 import { getFooter, getHeader, getPages, getTenant } from '@/lib/api'
-import ThemeRegistry from '@/providers/ThemeRegistry'
 import { notFound } from 'next/navigation'
 import '../globals.css'
+import { ThemeRegistry } from '@/providers/ThemeRegistry'
 
 export default async function DynamicPage({ params }: { params: { slug?: string[] } }) {
   const ppageParams = await params
@@ -18,7 +18,7 @@ export default async function DynamicPage({ params }: { params: { slug?: string[
   return (
     <html lang="en">
       <body>
-        <ThemeRegistry themeKey={tenant?.theme || 'green'}>
+        <ThemeRegistry>
           <HeaderRenderer header={header} />
           <BlockRenderer blocks={page?.layout || page?.content} tenant={tenant?.slug} />
           <FooterRenderer footer={footer} />
