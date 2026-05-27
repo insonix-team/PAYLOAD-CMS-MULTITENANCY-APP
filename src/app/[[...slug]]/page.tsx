783d7e8697dce9154baf315aef7dba117fe9cac1
kuldeep <kuldeep@insonix.com>
2026-05-27 15:41:53 +0530

    minor fixes

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -2,25 +2,42 @@ import { BlockRenderer } from '@/components/BlockRenderer'
 import FooterRenderer from '@/components/footer/FooterRenderer'
 import HeaderRenderer from '@/components/headers/HeaderRenderer'
 import { getFooter, getHeader, getPages, getTenant } from '@/lib/api'
-import { notFound } from 'next/navigation'
-import '../globals.css'
 import { ThemeRegistry } from '@/providers/ThemeRegistry'
+import { notFound } from 'next/navigation'
+import '../../globals.css'
 
 export default async function DynamicPage({ params }: { params: { slug?: string[] } }) {
-  const ppageParams = await params
-  const slugArr = ppageParams?.slug || []
-  const pageSlug = slugArr?.join('/') || 'home'
-  const [tenant, pageData, header, footer] = await Promise.all([getTenant(), getPages(pageSlug), getHeader(), getFooter()])
-  const page = pageData?.docs?.[0]
+  const p = await params
 
-  if (!tenant || !page) return notFound()
+  const slugArr = p?.slug || []
+
+  const pageSlug = slugArr?.join('/') || 'home'
+
+  const tenantDetails = await getTenant()
+
+  if (!tenantDetails) {
+    return notFound()
+  }
+
+  const data = await getPages(pageSlug)
+
+  const page = data?.docs?.[0]
+
+  if (!page) {
+    return notFound()
+  }
+
+  const header = await getHeader()
+  const footer = await getFooter()
 
   return (
     <html lang="en">
       <body>
         <ThemeRegistry>
           <HeaderRenderer header={header} />
-          <BlockRenderer blocks={page?.layout || page?.content} tenant={tenant?.slug} />
+
+          <BlockRenderer blocks={page?.layout || page?.content} tenant={tenantDetails?.slug} />
+
           <FooterRenderer footer={footer} />
         </ThemeRegistry>
       </body>
