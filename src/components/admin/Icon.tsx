7e28eacb02d08adbe3271af333765806d31cd6fc
Bharti Chambyal <bharti@insonix.com>
2026-07-01 17:03:22 +0530

    update admin logo

---
+++ src/components/admin/Icon.tsx
@@ -0,0 +1,22 @@
+const Icon = () => (
+  <span
+    style={{
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 18,
+      height: 20,
+      borderRadius: 4,
+      backgroundColor: '#fff',
+      color: '#2563eb',
+      fontWeight: 700,
+      fontSize: 10,
+      fontFamily: 'system-ui',
+      lineHeight: 1,
+    }}
+  >
+    Inx
+  </span>
+);
+
+export default Icon;
